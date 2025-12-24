ba9505bc1d72496dc475558927cfed5c4f342b58
Pigeon <146737777+pid-j@users.noreply.github.com>
2025-12-24 19:18:57 +0300

    aktsaa

--- CHRFNS.py
+++ CHRFNS.py
@@ -1,11 +1,8 @@
 import json, requests, urllib.parse
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
-page_cache = {}
-
 class Handler(BaseHTTPRequestHandler):
         def do_GET(self):
-                global page_cache
                 path = urllib.parse.urlparse(self.path).path
                 path = path.replace("%2F", "/").replace("%3A", ":")
 
@@ -13,15 +10,11 @@ class Handler(BaseHTTPRequestHandler):
                 self.send_header("Content-Type", "application/json")
                 self.send_header("Access-Control-Allow-Origin", "*")
                 self.end_headers()
-                if path in page_cache.keys():
-                    self.wfile.write(bytes(page_cache[path], "utf-8"))
-                else:
-                    r = requests.get(path).text.replace(
-                        "\n", "\\n").replace(
-                        "\r", "\\r").replace(
-                        "\t", "\\t").strip()
-                    self.wfile.write(bytes(json.dumps({"result": r}), "utf-8"))
-                    page_cache[path] = json.dumps({"result": r})
+                r = requests.get(path).text.replace(
+                    "\n", "\\n").replace(
+                    "\r", "\\r").replace(
+                    "\t", "\\t").strip()
+                self.wfile.write(bytes(json.dumps({"result": r}), "utf-8"))
 
 def main() -> None:
     print("CHRFNS - ChickenSuite HTML Requests From Native Scratch")
