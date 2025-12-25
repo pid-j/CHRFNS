4b7614575f4bc6c89802ebe29ac621961b9192a3
pid-j <ilikepigeons.whataboutyou@gmail.com>
2025-12-25 15:50:30 +0300

    p

--- CHRFNS.py
+++ CHRFNS.py
@@ -2,6 +2,12 @@ import json, requests
 import urllib.parse as urlp
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
+EXCEPTIONS = {
+    "chrfns://ping": "Pong. Hello from Syria!",
+    "chrfns://ver": "CHRFNS Server v1.1",
+    "chrfns://credits": "Created by @0x1194 on scratch.mit.edu",
+}
+
 class Handler(BaseHTTPRequestHandler):
         def do_GET(self):
                 path = urlp.urlparse(self.path).path
@@ -11,6 +17,17 @@ class Handler(BaseHTTPRequestHandler):
                 self.send_header("Content-Type", "application/json")
                 self.send_header("Access-Control-Allow-Origin", "*")
                 self.end_headers()
+
+                for e in EXCEPTIONS.keys():
+                    if path.startswith(e):
+                        f = EXCEPTIONS[e]
+                        if callable(f): f = f(path)
+                        r = f.replace(
+                            "\n", "\\n").replace(
+                            "\r", "\\r").replace(
+                            "\t", "\\t").strip()
+                        self.wfile.write(bytes(json.dumps({"result": r}), "utf-8"))
+                        return
                 r = requests.get(path).text.replace(
                     "\n", "\\n").replace(
                     "\r", "\\r").replace(
